7a08048b0095368a82696da38c625beb49475afa
dragonlost <dragonlost1@hotmail.fr>
2020-12-28 21:31:56 +0100

    correct ds1050 lib

--- Software/DS1050_lib.py
+++ Software/DS1050_lib.py
@@ -3,60 +3,47 @@ import smbus
 # open i2C bus number
 bus = smbus.SMBus(1)    # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
 
-DEVICE_ADDRESS = 0x29      # or 0x2b / 0x2F / 0x2A / 0x2E / 0x2D /0x2C
-bin_adress = "001"  # 010 / 100 / 011 / 111 / 110 / 101
+add_pwm_up = 0x2a
+add_pwm_down = 0x28
 
 
 pas = 100 / (2 ** 5)
 
-def set_pwm_DC(DC):
+def set_pwm_DC(DEVICE_ADDRESS, DC):
+
     # write
     dec_pwm = int(DC / pas)
 
-    bit_rw = "0"
     if 32 >= dec_pwm >= 0:
         print("ok")
         if dec_pwm == 32:
-            print("data : " + "00100000")
-            hex_data = hex(int("00000", 2))
-            hex_register = hex(int("001", 2))
-            bus.write_byte_data(DEVICE_ADDRESS, hex_register, hex_data)
+            bus.write_byte(DEVICE_ADDRESS, 0x20)
             print("set : 100%")
         else:
-            bin_pwm = f'{dec_pwm:05b}'
-            print("data : " + "000" + bin_pwm)
-            hex_data = hex(int(bin_pwm, 2))
-            hex_register = hex(int("000", 2))
-            bus.write_byte_data(DEVICE_ADDRESS, hex_register, hex_data)
+            bus.write_byte(DEVICE_ADDRESS, dec_pwm)
             print("set : " + str(dec_pwm * pas)+"%")
     else:
         print("nope")
 
 
-def read_pwm_DC():
+def read_pwm_DC(DEVICE_ADDRESS):
     # read
-    hex_register = hex(int("000", 2))
-    DC_bin = bus.read_byte_data(DEVICE_ADDRESS, hex_register)
-    DC = DC_bin * pas
-    print("data : " + str(DC_bin))
+    DC = bus.read_byte(DEVICE_ADDRESS)
+    print("data : " + str(DC * pas)+"%")
     return DC
 
 
-def set_Shutdown_mode():
+def set_Shutdown_mode(DEVICE_ADDRESS):
     # write
-    hex_data = hex(int("00000", 2))
-    hex_register = hex(int("110", 2))
-    bus.write_byte_data(DEVICE_ADDRESS, hex_register, hex_data)
+    bus.write_byte(DEVICE_ADDRESS, 0xC0)
     print("Shutdown ON")
 
     return 1
 
 
-def set_recall_mode():
+def set_recall_mode(DEVICE_ADDRESS):
     # write
-    hex_data = hex(int("00000", 2))
-    hex_register = hex(int("100", 2))
-    bus.write_byte_data(DEVICE_ADDRESS, hex_register, hex_data)
+    bus.write_byte(DEVICE_ADDRESS, 0x80)
     print("Shutdown OFF")
 
     return 1
