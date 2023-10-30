eae4a845b14009a6aff0f1cfa0ed6323c8a92f81
dragonlost <dragonlost1@hotmail.fr>
2023-10-31 00:44:18 +0100

    Update fan_pwm.py

--- Software/fan_pwm.py
+++ Software/fan_pwm.py
@@ -1,65 +1,77 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import RPi.GPIO as GPIO
 import time
-import signal
 import sys
-import os
 
 # Configuration
+FAN_PIN = 21  # BCM pin used to drive transistor's base
 WAIT_TIME = 1  # [s] Time to wait between each refresh
+FAN_MIN = 20  # [%] Fan minimum speed.
+PWM_FREQ = 25  # [Hz] Change this value if fan has strange behavior
 
+# Configurable temperature and fan speed steps
+tempSteps = [50, 70]  # [°C]
+speedSteps = [0, 100]  # [%]
 
-# Configurable temperature and fan speed
-MIN_TEMP = 40
-MAX_TEMP = 70
-FAN_LOW = 40
-FAN_HIGH = 60
-FAN_OFF = 0
-FAN_MAX = 1023
+# Fan speed will change only of the difference of temperature is higher than hysteresis
+hyst = 1
 
+# Setup GPIO pin
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(FAN_PIN, GPIO.OUT, initial=GPIO.LOW)
+fan = GPIO.PWM(FAN_PIN, PWM_FREQ)
+fan.start(0)
 
-# Get CPU's temperature
-def getCpuTemperature():
-    res = os.popen('vcgencmd measure_temp').readline()
-    temp = (res.replace("temp=", "").replace("'C\n", ""))
-    temp = float(temp)
-    return temp
+i = 0
+cpuTemp = 0
+fanSpeed = 0
+cpuTempOld = 0
+fanSpeedOld = 0
 
-
-# Set fan speed
-def setFanSpeed(speed):
-    print("speed : "+str(speed))
-    os.system('gpio pwm 26 '+str(speed))
-    return ()
-
-
-# Handle fan speed
-def handleFanSpeed():
-    temp = float(getCpuTemperature())
-    # Turn off the fan if temperature is below MIN_TEMP
-    if temp < MIN_TEMP:
-        setFanSpeed(FAN_OFF)
-        # print("Fan OFF") # Uncomment for testing
-    # Set fan speed to MAXIMUM if the temperature is above MAX_TEMP
-    elif temp > MAX_TEMP:
-        setFanSpeed(FAN_MAX)
-        # print("Fan MAX") # Uncomment for testing
-    # Caculate dynamic fan speed
-    else:
-        step = (FAN_HIGH - FAN_LOW) / (MAX_TEMP - MIN_TEMP)
-        temp -= MIN_TEMP
-        setFanSpeed(FAN_LOW + (round(temp) * step))
-        # print(FAN_LOW + ( round(temp) * step )) # Uncomment for testing
-    return ()
-
+# We must set a speed value for each temperature step
+if len(speedSteps) != len(tempSteps):
+    print("Numbers of temp steps and speed steps are different")
+    exit(0)
 
 try:
-    # Setup GPIO pin
-    os.system('gpio mode 26 pwm')
-    # Handle fan speed every WAIT_TIME sec
-    while True:
-        handleFanSpeed()
+    while 1:
+        # Read CPU temperature
+        cpuTempFile = open("/sys/class/thermal/thermal_zone0/temp", "r")
+        cpuTemp = float(cpuTempFile.read()) / 1000
+        cpuTempFile.close()
+
+        # Calculate desired fan speed
+        if abs(cpuTemp - cpuTempOld) > hyst:
+            # Below first value, fan will run at min speed.
+            if cpuTemp < tempSteps[0]:
+                fanSpeed = speedSteps[0]
+            # Above last value, fan will run at max speed
+            elif cpuTemp >= tempSteps[len(tempSteps) - 1]:
+                fanSpeed = speedSteps[len(tempSteps) - 1]
+            # If temperature is between 2 steps, fan speed is calculated by linear interpolation
+            else:
+                for i in range(0, len(tempSteps) - 1):
+                    if (cpuTemp >= tempSteps[i]) and (cpuTemp < tempSteps[i + 1]):
+                        fanSpeed = round((speedSteps[i + 1] - speedSteps[i])
+                                         / (tempSteps[i + 1] - tempSteps[i])
+                                         * (cpuTemp - tempSteps[i])
+                                         + speedSteps[i], 1)
+
+            if fanSpeed != fanSpeedOld:
+                if (fanSpeed != fanSpeedOld
+                        and (fanSpeed >= FAN_MIN or fanSpeed == 0)):
+                    fan.ChangeDutyCycle(fanSpeed)
+                    fanSpeedOld = fanSpeed
+            cpuTempOld = cpuTemp
+
+        # Wait until next refresh
         time.sleep(WAIT_TIME)
 
-except KeyboardInterrupt:  # trap a CTRL+C keyboard interrupt
-    os.system('gpio mode 26 out')
-    os.system('gpio out 26 1')
-    # GPIO.cleanup() # resets all GPIO ports used by this function
+
+# If a keyboard interrupt occurs (ctrl + c), the GPIO is set to 0 and the program exits.
+except KeyboardInterrupt:
+    print("Fan ctrl interrupted by keyboard")
+    GPIO.cleanup()
+    sys.exit()
